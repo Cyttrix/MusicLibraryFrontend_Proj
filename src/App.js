f9ec050ae9d14b026d45e0e230bc0b92ffa33dad
Ari Meyer <ari.f.meyer@gmail.com>
2023-10-13 11:29:44 -0500

    3rd commit, hooked up front end to backend and api, added components, data from api not rendered yet.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import NewSongItem from "./Components/NewSongItem/NewSongItem";
 
 function App() {
+  const [songs, setSongs] = useState([]);
+
+  const fetchSongs = async () => {
+    try {
+      const response = await axios.get("https://localhost:7142/api/Songs");
+      // console.log(response);
+      setSongs(response.data);
+    } catch (error) {
+      console.warn("Error in fetchSongs request: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+
+  const handleNewSong = (newSong) => {
+    const updatedSongs = [...songs, newSong];
+    setSongs(updatedSongs);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header */}
+      {/*  */}
+      <NewSongItem onNewSong={handleNewSong} />
     </div>
   );
 }
---
+++ src/Components/NewSongItem/NewSongItem.jsx
@@ -0,0 +1,9 @@
+const NewSongItem = ({}) => {
+    return ( 
+        <header>
+            <h2>Songs</h2>
+        </header>
+     );
+}
+ 
+export default NewSongItem;
